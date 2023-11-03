180915491da5e44b593eb77832c044f2a4d05397
Admin <Admin@DESKTOP-IEPVL3O>
2023-11-03 09:19:21 +0530

    third commit

---
+++ secondproject/src/main/java/secondproject/Exception.java
@@ -0,0 +1,14 @@
+package secondproject;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		try {
+		int sum=10/0;
+		}
+		catch(ArithmeticException e) {System.out.println("not possible");
+
+
+	}
+System.out.println("rest of the code");
+}}
